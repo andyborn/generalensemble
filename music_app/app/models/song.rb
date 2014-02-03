253436d56cc4995d3a6a16b980d0111f3d494462
Andy Born <andybornx@gmail.com>
2014-02-03 18:19:22 +0000

    added uploading song files to song object

--- music_app/app/models/song.rb
+++ music_app/app/models/song.rb
@@ -1,5 +1,10 @@
 class Song < ActiveRecord::Base
-  attr_accessible :about, :image, :lyrics, :title, :user_id
+
+
+  attr_accessible :about, :image, :lyrics, :title, :user_id, :song_file
+
+  mount_uploader :song_file, SongFileUploader
+
   belongs_to :user
 
   validates :title, presence: true
