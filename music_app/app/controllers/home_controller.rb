562e7089a5981b20bdac1ce6c00a0eb91c75d063
Andy Born <andybornx@gmail.com>
2014-02-06 15:11:19 +0000

    working towards custom background colour

--- music_app/app/controllers/home_controller.rb
+++ music_app/app/controllers/home_controller.rb
@@ -1,9 +1,10 @@
 class HomeController < ApplicationController
   def index
-    @users = User.artists
-    @songs = Song.all
-    @comments = Comment.all
-    @tags = ActsAsTaggableOn::Tag.all(:order=>'name')
+    @users = User.artists.order('created_at desc').limit(12)
+    @songs = Song.order('created_at desc').limit(12)
+    @comments = Comment.order('created_at desc').limit(5)
+    # @tags = ActsAsTaggableOn::Tag.all(:order=>'name')
+    @tags = User.tag_counts_on(:tags).order('count desc').limit(50)
 
 
 
