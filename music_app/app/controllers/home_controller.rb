fad21cd36ef065431f36e3d8341f26cea7349731
Andy Born <andybornx@gmail.com>
2014-02-06 20:06:48 +0000

    fixed size of home page

--- music_app/app/controllers/home_controller.rb
+++ music_app/app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
-    @users = User.artists.order('created_at desc').limit(12)
-    @songs = Song.order('created_at desc').limit(12)
+    @users = User.artists.order('created_at desc').limit(8)
+    @songs = Song.order('created_at desc').limit(8)
     @comments = Comment.order('created_at desc').limit(5)
     # @tags = ActsAsTaggableOn::Tag.all(:order=>'name')
     @tags = User.tag_counts_on(:tags).order('count desc').limit(50)
